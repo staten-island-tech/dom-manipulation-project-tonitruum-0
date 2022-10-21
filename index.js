a08f73bac3e29ed06fa90358633878bc7abd9d89
tonitruum-0 <104451675+tonitruum-0@users.noreply.github.com>
2022-10-21 11:37:51 -0400

    hoiguyftdresdxcfghjiko

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ let DOMSelectors = {};
 
 let inputArray = [];
 let nameArray = [];
-let conatiner = document.getElementById("container");
+let conatiner = document.getElementById("cardContainer");
+let cardNum = 0;
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -10,31 +11,39 @@ form.addEventListener("submit", function (e) {
   //name = user input
   // value = input name (same as id)
   for (let [name, value] of data) {
-    document.getElementById(name).value = "";
+    //document.getElementById(name).value = "";
     inputArray.push(value);
     nameArray.push(name);
   }
+  cardNum++;
   printCard();
 });
 
 function printCard() {
+  let card = document.createElement('div');
+  card.id = `card-${cardNum}`;
+  card.setAttribute("class", "card created");
+
+
   let title = document.createElement("p");
   title.textContent = inputArray[0];
-  title.id = "title";
-  title.setAttribute("class", "created");
+  title.id = `title-${cardNum}`;
+  title.setAttribute("class", "created title");
+  console.log(cardNum);
 
   let dsc = document.createElement("p");
   dsc.textContent = inputArray[1];
-  dsc.id = "dsc";
-  dsc.setAttribute("class", "created");
+  dsc.id = `dsc-${cardNum}`;
+  dsc.setAttribute("class", "created dsc");
 
   let imgLink = document.createElement("img");
   imgLink.src = inputArray[2];
+  imgLink.alt = `image${cardNum}`;
   imgLink.id = "img";
-  imgLink.setAttribute("class", "created");
+  imgLink.setAttribute("class", "created img");
 
-  conatiner.appendChild(title);
-  conatiner.appendChild(dsc);
-  conatiner.appendChild(imgLink);
+  conatiner.append(card);
+  card.append(title, dsc, imgLink);
+  console.log("success");
 }
 
