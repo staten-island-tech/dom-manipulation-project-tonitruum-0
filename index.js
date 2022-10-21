a52b0f06ba1a2acd1a0c653674f5fff1aecb220c
tonitruum-0 <104451675+tonitruum-0@users.noreply.github.com>
2022-10-21 00:13:30 -0400

    woooo

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 let DOMSelectors = {};
+
+let inputArray = [];
+let nameArray = [];
+let conatiner = document.getElementById("container");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const data = new FormData(form);
+  //name = user input
+  // value = input name (same as id)
+  for (let [name, value] of data) {
+    //document.getElementById(name).value = "";
+    inputArray.push(value);
+    nameArray.push(name);
+  }
+  printCard();
+});
+
+function printCard() {
+  let title = document.createElement("p");
+  title.textContent = inputArray[0];
+  title.id = "title";
+  title.setAttribute("class", "created");
+
+  let dsc = document.createElement("p");
+  dsc.textContent = inputArray[1];
+  dsc.id = "dsc";
+  dsc.setAttribute("class", "created");
+
+  let imgLink = document.createElement("img");
+  imgLink.src = inputArray[2];
+  imgLink.id = "img";
+  imgLink.setAttribute("class", "created");
+
+  conatiner.appendChild(title);
+  conatiner.appendChild(dsc);
+  conatiner.appendChild(imgLink);
+}
+
