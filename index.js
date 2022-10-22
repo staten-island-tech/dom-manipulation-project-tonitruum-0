a59ebde4b998236528cde87b23b8f0a2e6ab80d0
tonitruum-0 <104451675+tonitruum-0@users.noreply.github.com>
2022-10-21 23:24:35 -0400

    yesg

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ let inputArray = [];
 let nameArray = [];
 let conatiner = document.getElementById("cardContainer");
 let cardNum = 0;
+let blankFields = "false";
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   const data = new FormData(form);
-  //name = user input
-  // value = input name (same as id)
+  //value = user input
+  //name = input name (same as id)
   for (let [name, value] of data) {
     document.getElementById(name).value = "";
     inputArray.push(value);
     nameArray.push(name);
   }
+
   cardNum++;
   printCard();
 });
@@ -28,20 +30,31 @@ function printCard() {
   title.textContent = inputArray[0];
   title.id = `title-${cardNum}`;
   title.setAttribute("class", "created title");
-  console.log(cardNum);
 
   let dsc = document.createElement("p");
   dsc.textContent = inputArray[1];
   dsc.id = `dsc-${cardNum}`;
   dsc.setAttribute("class", "created dsc");
 
-  let imgLink = document.createElement("img");
-  imgLink.src = inputArray[2];
-  imgLink.alt = `image${cardNum}`;
-  imgLink.id = "img";
-  imgLink.setAttribute("class", "created img");
+  let imageValue = "";
+
+  if (inputArray[2] == "" || !inputArray[2].includes("http")) {
+  } else {
+    let imgLink = document.createElement("img");
+    imgLink.src = inputArray[2];
+    imgLink.alt = `image${cardNum}`;
+    imgLink.id = "img";
+    imgLink.setAttribute("class", "created img");
+    imageValue = imgLink;
+  }
 
   conatiner.append(card);
-  card.append(title, imgLink, dsc);
-  console.log("success");
+  card.append(title, imageValue, dsc);
+  inputArray = [];
+  nameArray = [];
+}
+
+
+function refresh() {
+  document.getElementById("cardContainer").innerHTML="";
 }
